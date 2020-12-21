b9c80fe6d9d16ed089a1d5f4f5b37ff18a7c2020
Adrian Babieczko <adrian.babieczko@gmail.com>
2020-12-21 13:16:35 +0100

    Generate tag lists

--- js/script.js
+++ js/script.js
@@ -78,28 +78,45 @@ function generateTitleLinks() {
 
 function generateTags(){
   /* find all articles */
+  const articles = document.querySelectorAll('.post');
 
   /* START LOOP: for every article: */
+  for (const article of articles) {
 
     /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
 
     /* make html variable with empty string */
+    let html = '';
 
     /* get tags from data-tags attribute */
+    let articleTags = article.getAttribute('data-tags');
 
     /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
 
     /* START LOOP: for each tag */
+    for (const tag of articleTagsArray) {
 
       /* generate HTML of the link */
+      const linkHTML =
+      ' <li><a href="#tag-' +
+      tag +
+      '">' +
+      tag +
+      '</a></li>';
 
       /* add generated code to html variable */
+      html = html + linkHTML;
 
     /* END LOOP: for each tag */
+    }
 
     /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
 
   /* END LOOP: for every article: */
+  }
 }
 
 {
